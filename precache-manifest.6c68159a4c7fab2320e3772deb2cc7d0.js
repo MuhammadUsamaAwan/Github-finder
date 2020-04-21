bbad0306be0ea5ddee0e273863b7a927be6081a2
Usama Awan <underusama@gmail.com>
2020-04-21 21:57:57 +0500

    Updates

---
+++ precache-manifest.6c68159a4c7fab2320e3772deb2cc7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04844695f34a0acffa0094513cd38064",
+    "url": "/Github-finder/index.html"
+  },
+  {
+    "revision": "6122365ce8febfdc8c62",
+    "url": "/Github-finder/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6122365ce8febfdc8c62",
+    "url": "/Github-finder/static/js/2.8216a2f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Github-finder/static/js/2.8216a2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420528678bafb1d7c049",
+    "url": "/Github-finder/static/js/main.91dbad26.chunk.js"
+  },
+  {
+    "revision": "17182fdbf8002ec06410",
+    "url": "/Github-finder/static/js/runtime-main.ccd1a5ee.js"
+  }
+]);
